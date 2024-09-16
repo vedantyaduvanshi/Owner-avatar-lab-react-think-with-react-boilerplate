ffaa91661d9bf98a99c3afba454fe0e13c4b741b
Vedant Yaduvanshi-) <87130565+vedantyaduvanshi@users.noreply.github.com>
2024-09-17 04:07:49 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+// const container = document.getElementById('react-container');
+
+// ReactDOM.render("Hello! Welcome to React",container);
+
+// const Container = () =>{
+//     return React.createElement(`div`,null,`Hey Kalvians! Welcome to React Learning`,
+//        React.createElement(`div`,null,`Let's rock and roll`)
+//        );
+// }
+// // Render the Container
+// const container = document.getElementById('react-container');
+// ReactDOM.render(React.createElement(Container),container);
+
+// class component
+
+class ReactContainer extends React.Component{
+    // constructor - to initialize the state 
+  
+    // render method to render the react dom 
+      render(){
+          return React.createElement(`div`,null,`Hey Kalvians`,
+          React.createElement(`div`,null,`Let's rock and roll with classes`)
+          );
+      }
+    
+  }
+  const container = document.getElementById('react-container');
+  ReactDOM.render(React.createElement(ReactContainer),container);
